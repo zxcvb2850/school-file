1d849a405f07c13deacb07b7de7f26c617e771a2
myPc <zxcvb2850@163.com>
2018-05-29 23:49:50 +0800

    react-router-dom 添加路由

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
-import {getToken, storageToken, clearLocal} from "./redux/login/actions";
+import {Link} from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <p onClick={() => this.props.storageToken("login", "wang1234")}>储存token</p>
-                <p onClick={() => this.props.getToken("login")}>------{this.props.token}</p>
-                <p onClick={this.props.clearLocal}>清除所有localStorage</p>
+                <Link to={"/"}>Home</Link>
+                <Link to={"/login"}>Login</Link>
+                <Link to={"/my"}>My</Link>
             </div>
         );
     }
 }
 
-const mapStateProps = (state) => {
-    console.log(state.loginStatus.token);
-    return {
-        token: state.loginStatus.token
-    }
-}
-
-const mapDispatchProps = {
-    getToken,
-    storageToken,
-    clearLocal,
-};
-
-export default connect(mapStateProps, mapDispatchProps)(App);
+export default connect()(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import App from './App';
+import Route from "./router/";
 import "./common/styles/index.less"
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+const render = Component => {
+    ReactDOM.render(<Provider store={store}><Component/></Provider>, document.getElementById('root'));
+}
+render(Route);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+    module.hot.accept('./router/', () => {
+        render(Route);
+    })
+}
+
 registerServiceWorker();
